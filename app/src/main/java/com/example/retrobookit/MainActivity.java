6fc4d135660d3d3fb935b42e6beed0f3e24faea6
Sameer Ahmad <sahmad737@gmail.com>
2020-10-02 03:01:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrobookit/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.retrobookit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.retrobookit.controller.CreateAccountActivity;
+import com.example.retrobookit.controller.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button signUpButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton=findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        signUpButton=findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/retrobookit/controller/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.retrobookit.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.retrobookit.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
