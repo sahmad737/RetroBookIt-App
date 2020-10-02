018c84ff4cfd25324b9181828276ea80902efa42
Sameer Ahmad <sahmad737@gmail.com>
2020-10-02 19:06:39 +0530

    Added Home Activity

---
+++ app/src/main/java/com/example/retrobookit/controller/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.retrobookit.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.retrobookit.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
