f77636ab348a70c8dfb704731a551e26651e84bb
Sameer Ahmad <sahmad737@gmail.com>
2020-10-03 18:16:57 +0530

    Added Camera functionality to Upload book image

--- app/src/main/java/com/example/retrobookit/controller/UploadBookActivity.java
+++ app/src/main/java/com/example/retrobookit/controller/UploadBookActivity.java
@@ -2,15 +2,56 @@ package com.example.retrobookit.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.retrobookit.R;
 
 public class UploadBookActivity extends AppCompatActivity {
+        private Bundle extras;
+        private Bitmap imageBitmap;
+        private Button uploadPhotoButton;
+        private ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_book);
+
+        uploadPhotoButton=findViewById(R.id.uploadButton);
+        imageView=findViewById(R.id.bookPostImage);
+        uploadPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    
 }
