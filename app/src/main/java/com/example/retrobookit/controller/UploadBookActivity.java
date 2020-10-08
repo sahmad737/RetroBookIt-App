9d0ce4dbd08eb425f0e443ac1bef68147c5bad50
Sameer Ahmad <sahmad737@gmail.com>
2020-10-08 19:45:23 +0530

    Added Camera functionality to Upload book image

--- app/src/main/java/com/example/retrobookit/controller/UploadBookActivity.java
+++ app/src/main/java/com/example/retrobookit/controller/UploadBookActivity.java
@@ -9,15 +9,25 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.retrobookit.R;
+import com.example.retrobookit.data.BookData;
+import com.example.retrobookit.model.Book;
 
 public class UploadBookActivity extends AppCompatActivity {
         private Bundle extras;
         private Bitmap imageBitmap;
         private Button uploadPhotoButton;
         private ImageView imageView;
+        private EditText uploadName;
+        private EditText uploadAuthor;
+        private EditText uploadPrice;
+        private EditText uploadDescription;
+        private Button postButton;
+        private BookData bookData=new BookData();
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
@@ -31,6 +41,24 @@ public class UploadBookActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+            }
+        });
+
+        //fetching rest of the views
+        uploadName=findViewById(R.id.nameEditTextAddress);
+        uploadAuthor=findViewById(R.id.authorEditTextAddress);
+        uploadPrice=findViewById(R.id.priceEditTextAddress);
+        uploadDescription=findViewById(R.id.descriptionEditTextAddress);
+        postButton=findViewById(R.id.postButton);
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book= new Book(""+uploadName.getText(),""+uploadAuthor.getText(),"Price= Rs"+uploadPrice.getText(),
+                            ""+uploadDescription.getText(),imageView.getId()
+                        );
+                    bookData.addBook(book);
+                    startActivity(new Intent(UploadBookActivity.this,BookListActivity.class));
             }
         });
 
@@ -49,7 +77,7 @@ public class UploadBookActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+             imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
         }
     }
--- app/src/main/java/com/example/retrobookit/data/BookData.java
+++ app/src/main/java/com/example/retrobookit/data/BookData.java
@@ -11,14 +11,19 @@ import java.util.ArrayList;
 
 public class BookData {
     private String lorem= "lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum";
+    private static ArrayList<Book> list=new ArrayList<>();
+
     public ArrayList<Book> bookList(){
-        ArrayList<Book> list=new ArrayList<>();
         for(int i=0;i<16;i++){
             Book book=new Book("Book"+(i+1),"Author Name",
                     "Price: Rs 420", lorem,R.drawable.book);
             list.add(book);
         }
         return list;
+    }
+
+    public void addBook(Book book){
+        list.add(book);
     }
 
 }
