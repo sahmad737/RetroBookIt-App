bb8aea5a8d37e50e4c20a2edc4eb36b3d822d374
Sameer Ahmad <sahmad737@gmail.com>
2021-02-18 23:26:06 +0530

    Added Login and Sign Up functionality

--- app/src/main/java/com/example/retrobookit/controller/CreateAccountActivity.java
+++ app/src/main/java/com/example/retrobookit/controller/CreateAccountActivity.java
@@ -1,19 +1,31 @@
 package com.example.retrobookit.controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.retrobookit.MainActivity;
 import com.example.retrobookit.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     private Button createAccountButton;
+    private EditText email;
+    private EditText password;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +33,38 @@ public class CreateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
 
         createAccountButton=findViewById(R.id.createAccountButton);
+        email= findViewById(R.id.createActUsername);
+        password=findViewById(R.id.createActPassword);
+        mAuth=FirebaseAuth.getInstance();
+
+
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(CreateAccountActivity.this,"Account Created",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
+                String emailString= email.getText().toString();
+                String passwordString=password.getText().toString();
+
+                if(!emailString.equals("") && !passwordString.equals("")){
+                    mAuth.createUserWithEmailAndPassword(emailString,passwordString)
+                            .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()) {
+                                        Toast.makeText(CreateAccountActivity.this,"Failed Sign Up",Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        Toast.makeText(CreateAccountActivity.this,"Account Created",Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
+
+                                    }
+                                }
+                            });
+
+
+                }
+
+
+
             }
         });
 
--- app/src/main/java/com/example/retrobookit/controller/LoginActivity.java
+++ app/src/main/java/com/example/retrobookit/controller/LoginActivity.java
@@ -1,27 +1,67 @@
 package com.example.retrobookit.controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.retrobookit.MainActivity;
 import com.example.retrobookit.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
     private Button loginButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-         loginButton=findViewById(R.id.loginButton);
+            loginButton=findViewById(R.id.loginButton);
+            email= findViewById(R.id.loginUsername);
+            password=findViewById(R.id.loginPassword);
+            mAuth=FirebaseAuth.getInstance();
 
          loginButton.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                 String emailString= email.getText().toString();
+                 String passwordString=password.getText().toString();
+
+                 if(!emailString.equals("") && !passwordString.equals("")) {
+
+                     mAuth.signInWithEmailAndPassword(emailString,passwordString)
+                             .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                 @Override
+                                 public void onComplete(@NonNull Task<AuthResult> task) {
+                                     if(!task.isSuccessful()) {
+                                         Toast.makeText(LoginActivity.this,"Failed Sign In",Toast.LENGTH_LONG).show();
+                                     }
+                                     else{
+                                         startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                                         Toast.makeText(LoginActivity.this,"Successfully Sign In",Toast.LENGTH_LONG).show();
+
+                                     }
+                                 }
+                             });
+                 }
+
+
+
              }
          });
 
